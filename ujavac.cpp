86e0622be694ca1485763f603837168e6045e6b6
xvrc <xvrc.dev@pm.me>
2023-06-22 10:22:25 -0400

    Begin options parsing

--- ujavac.cpp
+++ ujavac.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstring>
 #include <format>
 #include <utility>
 
@@ -28,33 +29,58 @@ bool is_file_terminal(std::FILE *f)
 #endif
 }
 
+template <class... Args> u32 print(std::FILE *f, std::format_string<Args...> fmt, Args &&...args)
+{
+    int ret =
+        std::fprintf(f, "%s", std::vformat(fmt.get(), std::make_format_args(std::forward<Args>(args)...)).c_str());
+    return ret < 0 ? 0 : ret;
+}
+
 template <class... Args> u32 print(std::format_string<Args...> fmt, Args &&...args)
 {
-    int ret = std::printf("%s", std::vformat(fmt.get(), std::make_format_args(std::forward<Args>(args)...)).c_str());
+    return print(stdout, fmt, std::forward<Args>(args)...);
+}
+
+template <class... Args> u32 println(std::FILE *f, std::format_string<Args...> fmt, Args &&...args)
+{
+    int ret =
+        std::fprintf(f, "%s\n", std::vformat(fmt.get(), std::make_format_args(std::forward<Args>(args)...)).c_str());
     return ret < 0 ? 0 : ret;
 }
 
 template <class... Args> u32 println(std::format_string<Args...> fmt, Args &&...args)
 {
-    int ret = std::printf("%s\n", std::vformat(fmt.get(), std::make_format_args(std::forward<Args>(args)...)).c_str());
-    return ret < 0 ? 0 : ret;
+    return println(stdout, fmt, std::forward<Args>(args)...);
 }
 
-struct prog_opt_store
+enum class prog_opt
 {
+    help,
+    system,
+    verbose,
+    version,
+    werror
+};
+
+struct prog_opt_desc
+{
+    prog_opt id;
     const char *keys[3];
     const char *help = nullptr;
     const char *param = nullptr;
     bool extra = false;
 };
 
-constexpr prog_opt_store prog_opts[] = {{{"--system"}, "Override location of system modules", "<jdk>|none"},
-                                        {
-                                            {"-verbose"},
-                                            "Output mesages about what the compiler is doing",
-                                        },
-                                        {{"--version", "-version"}, "Version information"},
-                                        {{"-Werror"}, "Terminate compilation if warnings occur"}};
+constexpr prog_opt_desc prog_opt_descs[] = {
+    {prog_opt::help, {"--help", "-help", "-?"}, "Show this help message"},
+    {prog_opt::system, {"--system"}, "Override location of system modules", "<jdk>|none"},
+    {
+        prog_opt::verbose,
+        {"-verbose"},
+        "Output mesages about what the compiler is doing",
+    },
+    {prog_opt::version, {"--version", "-version"}, "Version information"},
+    {prog_opt::werror, {"-Werror"}, "Terminate compilation if warnings occur"}};
 
 u8 compile()
 {
@@ -62,34 +88,45 @@ u8 compile()
     return 0;
 }
 
-void show_help()
+void print_usage(std::FILE* f)
 {
-    println("Usage: ujavac <options> <source files>");
+    println(f, "Usage: ujavac <options> <source files>");
+}
+
+void print_usage()
+{
+    print_usage(stdout);
+}
+
+void print_help()
+{
+    print_usage();
     println("where possible options include:");
 
-    for (u32 i = 0; i < std::size(prog_opts); i++)
+    for (auto &desc : prog_opt_descs)
     {
         u32 line_len = 0;
-        const auto &opt = prog_opts[i];
 
-        for (u32 j = 0; j < std::size(opt.keys); j++)
+        for (u32 i = 0; i < std::size(desc.keys); i++)
         {
-            auto key = opt.keys[j];
+            auto key = desc.keys[i];
             if (!key)
             {
                 break;
             }
 
-            if (j == 0)
+            if (i == 0)
             {
                 line_len += print("  ");
-            } else {
+            }
+            else
+            {
                 line_len += print(", ");
             }
 
-            if (opt.param)
+            if (desc.param)
             {
-                line_len += print("{} {}", key, opt.param);
+                line_len += print("{} {}", key, desc.param);
             }
             else
             {
@@ -112,8 +149,13 @@ void show_help()
             print("      ");
         }
 
-        println("{}", opt.help);
+        println("{}", desc.help);
     }
+}
+
+void print_version()
+{
+    println("ujavac 1.0.0");
 }
 } // namespace
 
@@ -123,23 +165,45 @@ int main(int argc, char **argv)
     {
         if (is_file_terminal(stdout))
         {
-            show_help();
+            print_help();
         }
 
         return 2;
     }
 
-    bool opt_help = false;
-
     for (u32 i = 1; i < argc; i++)
     {
+        auto arg = argv[i];
+        for (auto &desc : prog_opt_descs)
+        {
+            for (auto &key : desc.keys)
+            {
+                if (key && !std::strcmp(key, arg))
+                {
+                    if (i == argc - 1 && desc.param)
+                    {
+                        println(stderr, "error: {} requires an argument", arg);
+                        print_usage(stderr);
+                        println("use --help for a list of possible options");
+                        return 1;
+                    }
 
-    }
+                    switch (desc.id)
+                    {
+                    case prog_opt::help:
+                        print_help();
+                        return 0;
+                    case prog_opt::version:
+                        print_version();
+                        return 0;
+                    }
 
-    if (opt_help)
-    {
-        show_help();
-        return 0;
+                    goto outer;
+                }
+            }
+        }
+
+    outer:;
     }
 
     return compile();
