4fc9edb0d60e302130f2830e5514c549d7cde589
xvrc <xvrc.dev@pm.me>
2023-06-28 18:02:21 -0400

    Implement basic comment skipping

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -2,6 +2,7 @@
 
 #include <atomic>
 #include <cstdio>
+#include <cstring>
 
 #include <tbb/parallel_for.h>
 
@@ -10,34 +11,39 @@ constexpr bool is_dec_digit(u32 c)
     return c >= '0' && c <= '9';
 }
 
+constexpr bool is_identifier_ignorable(u32 c)
+{
+    return c <= 0x08 || c >= 0x0E && c <= 0x01B || c >= 0x7F && c <= 0x9F;
+}
+
 // JLS 3.8, "Java letter" definition
 // TODO add Unicode support
-constexpr bool is_java_iden_start(u32 c)
+constexpr bool is_identifier_start(u32 c)
 {
     return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '$' || c == '_';
 }
 
 // JLS 3.8, "Java letter-or-digit" definition
 // TODO add Unicode support
-constexpr bool is_java_iden_part(u32 c)
+constexpr bool is_identifier_part(u32 c)
 {
-    return is_java_iden_start(c) || is_dec_digit(c);
+    return is_identifier_start(c) || is_dec_digit(c) || is_identifier_ignorable(c);
 }
 
 // JLS 3.6
-constexpr bool is_java_whitespace(u32 c)
+constexpr bool is_whitespace(u32 c)
 {
     return c == ' ' || c == '\t' || c == '\f' || c == '\r' || c == '\n';
 }
 
-constexpr bool is_java_hex_digit(u32 c)
+constexpr bool is_hex_digit(u32 c)
 {
     return c >= 'A' && c <= 'F' || c >= 'a' && c <= 'f' || is_dec_digit(c);
 }
 
 constexpr bool is_ascii(u32 c)
 {
-    return c < 128;
+    return c < 0x80;
 }
 
 constexpr bool is_utf16_high_surrogate(u16 c)
@@ -50,6 +56,13 @@ constexpr bool is_utf16_low_surrogate(u16 c)
     return c >= 0xDC00 && c <= 0xDFFF;
 }
 
+enum class LexerItem
+{
+    None,
+    TraditionalComment,
+    EndOfLineComment,
+};
+
 bool Compiler::compile_input(u32 i) const
 {
     std::FILE *src_file = std::fopen(m_inputs[i], "rb");
@@ -57,7 +70,7 @@ bool Compiler::compile_input(u32 i) const
 
     // State that tracks the special end-of-file condition outlined in JLS 3.5
     int src_file_ch = 0;
-    bool prev_sub = false;
+    bool prev_raw_sub = false;
 
     // Reconstructed Unicode code point from UTF-8 input.
     u32 raw_unicode = 0;
@@ -65,31 +78,35 @@ bool Compiler::compile_input(u32 i) const
     // State counter for Unicode code point reconstruction.
     u32 raw_unicode_remaining = 0;
 
+    // Source code line segmentation state.
+    u64 line_num = 1;
+    u64 col_num = 1;
+    bool prev_raw_cr = false;
+
     // State variables responsible for keeping track of
     // Unicode escape sequences (i.e. \uXXXX).
     u16 esc_utf16[2] = {};
     u32 esc_utf16_len = 0;
     u32 esc_utf16_remaining = 0;
-    bool prev_esc = false;
+    bool prev_from_esc = false;
 
     // Backslashes need special care during parsing because
     // they're used in escape sequences at various stages.
     bool prev_backslash = false;
-
-    char token_buf[sizeof("synchronized")] = {};
-    u32 token_buf_len = 0;
-
-    // Source code line segmentation state.
-    u64 line_num = 1;
-    u64 col_num = 1;
-    bool prev_cr = false;
-
     u64 raw_backslash_count = 0;
+
+    char ascii_tok_buf[sizeof("synchronized")];
+    u32 ascii_tok_buf_len = 0;
+
+    LexerItem lexer_item = LexerItem::None;
+    bool prev_trad_comment_end_star = false;
 
     if (!src_file || !dst_file)
     {
         goto cleanup;
     }
+
+#define ascii_tok_buf_eq(other) (!std::strncmp(ascii_tok_buf, other, ascii_tok_buf_len))
 
     while (src_file_ch != EOF)
     {
@@ -97,12 +114,17 @@ bool Compiler::compile_input(u32 i) const
 
         // A single UTF-8 encoding character from the input
         // stream. All input starts from this representation.
-        // Use of the SUB character as a placeholder for EOF is
-        // inspired by JLS 3.5
-        u8 raw_utf8 = src_file_ch == EOF ? '\x1A' : src_file_ch;
+        // TODO handle EOF condition correctly
+        u8 raw_utf8 = src_file_ch == EOF ? 0 : src_file_ch;
 
         if (!raw_unicode_remaining)
         {
+            if (raw_utf8 == 0x80)
+            {
+                // TODO fatal
+                goto cleanup;
+            }
+
             for (raw_unicode_remaining = 1; raw_utf8 & (0x80 >> raw_unicode_remaining); ++raw_unicode_remaining)
                 ;
             raw_unicode = raw_utf8;
@@ -119,25 +141,21 @@ bool Compiler::compile_input(u32 i) const
 
         if (raw_unicode == '\x1A')
         {
-            prev_sub = true;
+            prev_raw_sub = true;
         }
 
         // JLS 3.4
-        if (raw_unicode == '\n')
-        {
-            if (!prev_cr)
-            {
-                line_num++;
-                col_num = 1;
-            }
-
-            prev_cr = false;
-        }
-        else if (raw_unicode == '\r')
+        if (raw_unicode == '\r' || raw_unicode == '\n' && !prev_raw_cr)
         {
             line_num++;
             col_num = 1;
-            prev_cr = true;
+
+            if (lexer_item == LexerItem::EndOfLineComment)
+            {
+                lexer_item = LexerItem::None;
+            }
+
+            prev_raw_cr = raw_unicode == '\r';
         }
         else
         {
@@ -198,7 +216,7 @@ bool Compiler::compile_input(u32 i) const
 
             if (!esc_utf16_remaining)
             {
-                prev_esc = true;
+                prev_from_esc = true;
                 esc_utf16_len++;
 
                 if (esc_utf16_len == 1 && is_utf16_low_surrogate(esc_utf16[0]))
@@ -218,7 +236,7 @@ bool Compiler::compile_input(u32 i) const
             esc_utf16_len = 0;
         }
         // JLS 3.3
-        else if (prev_backslash && raw_unicode == 'u' && (raw_backslash_count % 2 == 0 || prev_esc))
+        else if (prev_backslash && raw_unicode == 'u' && (raw_backslash_count % 2 == 0 || prev_from_esc))
         {
             esc_utf16_remaining = 4;
             prev_backslash = false;
@@ -231,7 +249,7 @@ bool Compiler::compile_input(u32 i) const
             continue;
         }
 
-        prev_esc = false;
+        prev_from_esc = false;
 
         if (esc_utf16_len && is_utf16_high_surrogate(esc_utf16[0]))
         {
@@ -239,10 +257,50 @@ bool Compiler::compile_input(u32 i) const
             goto cleanup;
         }
 
-        // print("{} ", unicode);
+        if (lexer_item == LexerItem::TraditionalComment)
+        {
+            if (prev_trad_comment_end_star && unicode == '/')
+            {
+                lexer_item = LexerItem::None;
+                prev_trad_comment_end_star = false;
+            }
+            else if (unicode == '*')
+            {
+                prev_trad_comment_end_star = true;
+            }
+
+            continue;
+        }
+
+        if (lexer_item == LexerItem::EndOfLineComment)
+        {
+            continue;
+        }
+
+        if (is_ascii(unicode))
+        {
+            ascii_tok_buf[ascii_tok_buf_len++] = unicode;
+
+            if (ascii_tok_buf_eq("//"))
+            {
+                lexer_item = LexerItem::EndOfLineComment;
+                ascii_tok_buf_len = 0;
+            }
+            else if (ascii_tok_buf_eq("/*"))
+            {
+                lexer_item = LexerItem::TraditionalComment;
+                ascii_tok_buf_len = 0;
+            }
+            // FIXME too preemptive, doesn't account for e.g. "constz", which is valid
+            else if (ascii_tok_buf_eq("const") || ascii_tok_buf_eq("goto"))
+            {
+                // TODO fatal
+                goto cleanup;
+            }
+        }
     }
 
-    // println("");
+#undef ascii_tok_buf_eq
 
 cleanup:
     bool good = false;
